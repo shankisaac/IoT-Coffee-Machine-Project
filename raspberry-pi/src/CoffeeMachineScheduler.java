31b1b71f742a226115783f1779e2fad49db10605
Shankar <92207120+shankisaac@users.noreply.github.com>
2024-09-10 16:24:49 +1000

    Update CoffeeMachineScheduler.java

--- raspberry-pi/src/CoffeeMachineScheduler.java
+++ raspberry-pi/src/CoffeeMachineScheduler.java
@@ -9,43 +9,50 @@ import java.util.TimerTask;
 public class CoffeeMachineScheduler {
 
     private static final String API_URL = "http://localhost:8080/schedule";
+    private final RelayControl relayControl;
+
+    public CoffeeMachineScheduler() {
+        this.relayControl = new RelayControl();
+    }
 
     public static void main(String[] args) {
+        CoffeeMachineScheduler scheduler = new CoffeeMachineScheduler();
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                try {
-                    URL url = new URL(API_URL);
-                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                    conn.setRequestMethod("GET");
-
-                    BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                    String inputLine;
-                    StringBuilder response = new StringBuilder();
-
-                    while ((inputLine = in.readLine()) != null) {
-                        response.append(inputLine);
-                    }
-                    in.close();
-
-                    String scheduleTime = response.toString();
-                    LocalTime now = LocalTime.now();
-                    LocalTime schedule = LocalTime.parse(scheduleTime);
-
-                    if (now.isAfter(schedule.minusMinutes(10)) && now.isBefore(schedule)) {
-                        startCoffeeMachine();
-                    }
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                scheduler.checkSchedule();
             }
         }, 0, 60000); // Check every minute
     }
 
-    private static void startCoffeeMachine() {
-        System.out.println("Starting coffee machine!");
-        // Code to control the relay module to start the coffee machine
+    private void checkSchedule() {
+        try {
+            URL url = new URL(API_URL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            String scheduleTime = response.toString();
+            LocalTime now = LocalTime.now();
+            LocalTime schedule = LocalTime.parse(scheduleTime);
+
+            if (now.isAfter(schedule.minusMinutes(10)) && now.isBefore(schedule)) {
+                relayControl.start();
+            } else {
+                relayControl.stop();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
